0450c74e3c49cb71a58ce9c27a6a49f0d766fa0b
Oleksii Suprun <alexey.suprun@raisin.com>
2022-04-25 12:33:47 +0200

    feat: adds close btn for congratulation modal

--- src/components/Congratulation.tsx
+++ src/components/Congratulation.tsx
@@ -1,11 +1,17 @@
+import {useState} from 'react'
+
 type Props = {
     roll: number,
     time: number
 }
 
 function Congratulation({roll, time}: Props) {
+
+    const [modal, setModal] = useState(true)
+
     return(
-        <div className="congratulation">
+        <div className={`congratulation ${modal ? 'open' : ''}`}>
+            <span className="close-btn" onClick={() => setModal(!setModal)}></span>
             <h3>Congratulations!</h3>
             <span>You resolved this puzzle in {roll} rolls and {time} seconds</span>
         </div>
